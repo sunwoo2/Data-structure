aa143eebc747ecfbd863cfbf6c76f29a75978daf
sunwoo2 <37177509+sunwoo2@users.noreply.github.com>
2018-11-19 19:39:59 +0900

    Update heap.c

--- Heap/heap.c
+++ Heap/heap.c
@@ -13,13 +13,13 @@ HEAP* create_heap(int max){
 bool reheapUp (HEAP* heap, int newNode){
 	if(heap->array+newNode == heap->array) return false;
 	
-	int parent = (newNode-1)/2;			// check index!
-	if( heap->array[newNode] > heap->array[parent] ){
+	int parent = (newNode-1)/2;			// check index!	  // 부모노드 찾아주기(Heap 구조 부모와 자식노드간의 관계!!!)
+	if( heap->array[newNode] > heap->array[parent] ){		// 자식노드가 더 크면 swap하는 원리
 		int temp;
 		temp = heap->array[newNode];
 		heap->array[newNode] = heap->array[parent];
 		heap->array[parent] = temp;
-		reheapUp(heap, parent);
+		reheapUp(heap, parent);				// recursively!!
 	}
 	return true;
 }
@@ -28,8 +28,8 @@ bool insertHeap (HEAP* heap, int data){
 	if(heap->size == heap->max_size) return false;
 
 	heap->last++;
-	heap->array[heap->last] = data;
-	reheapUp(heap, heap->last);
+	heap->array[heap->last] = data;			// Heap에서 삽입은 그냥 맨끝 빈자리에 삽입해주고, 그다음 Reheap 하는 알고리즘
+	reheapUp(heap, heap->last);			// Heap구조 유지 시켜주기
 
 	heap->size++;
 	return true;
@@ -37,7 +37,7 @@ bool insertHeap (HEAP* heap, int data){
 
 bool reheapDown (HEAP* heap, int root){
 	int left, right, large, temp;
-	left = root*2+1;	
+	left = root*2+1;			// 자식노드 두개 찾아주기
 	right= root*2+2;	
 	
 	if(left > heap->last) return false;		// that's leafnode
@@ -51,7 +51,7 @@ bool reheapDown (HEAP* heap, int root){
 		temp = heap->array[root];					// exchange root and largeSubtree
 		heap->array[root] = heap->array[large];
 		heap->array[large] = temp;
-		reheapDown(heap, large);
+		reheapDown(heap, large);			// recursively!!
 	}
 
 	return true;
